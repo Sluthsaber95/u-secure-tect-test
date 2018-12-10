c741ac8a4259f6e9474cfa2dbdd092726c204453
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-10 23:28:14 +0000

    Doc Button now shows iconography option

--- src/stories/Buttons/index.js
+++ src/stories/Buttons/index.js
@@ -4,6 +4,14 @@ import Button from '../../components/Button'
 
 const Buttons = () => storiesOf('Buttons', module)
   .add('Button Classic', () => <Button />)
-  .add('Button Inverted', () => <Button optionsUsed={true}/>)
+  .add('Button Inverted', () => <Button inverted={true} />)
+  .add('Buttons including Plus Icon', () => {
+    return (
+      <section style={{padding: 10}}>
+        <Button iconPresent={true} inverted={true} />
+        <Button iconPresent={true} />
+      </section>
+    )
+  })
 
 export default Buttons
