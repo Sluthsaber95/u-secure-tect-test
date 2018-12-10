68cbbb972245366c2259467d7027eccb371dfcc1
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-10 12:00:42 +0000

    Button Styles Completed

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import styled from '@emotion/styled'
+import globalStyles from '../../globalStyles'
 
-const ButtonOptions = ({ children }) => (<button>{children}</button>)
-const ButtonNormal = ({ children }) => (<button>{children}</button>)
+const { color, borderSize, borderRadius, typography } = globalStyles();
+const { curve } = borderRadius
+const { none, thin } = borderSize;
+const { black, blueDodger, white } = color;
+const { smReg } = typography
 
-const Button = ({ children, optionType }) => {
+const styleBtn = {
+  font: smReg,
+  padding: '10px 10px 5px 10px',
+  borderSizing: 'border-boxing',
+  borderRadius: curve,
+}
+
+const ButtonInverted = styled.button`
+  border: ${thin} solid ${blueDodger};
+  color: ${blueDodger};
+  background: ${white};
+`
+
+const ButtonNormal = styled.button`
+  border: ${none} solid ${black};
+  color: ${white};
+  background: ${blueDodger};
+`
+
+const Button = ({ children = 'Add Button Text', optionsUsed = false }) => {
   return (
-    <button>{children}</button>
+    <Fragment>
+      {
+        optionsUsed
+          ? <ButtonInverted style={styleBtn}>{children}</ButtonInverted>
+          : <ButtonNormal style={styleBtn}>{children}</ButtonNormal>
+      }
+    </Fragment>
   )
 }
 
