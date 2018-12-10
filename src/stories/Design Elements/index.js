5bb14396b562ad7bf4f0ed6466ac56e8fdab911b
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-10 12:02:13 +0000

    Include Additional Text Themes

--- src/stories/Design Elements/index.js
+++ src/stories/Design Elements/index.js
@@ -1,7 +1,13 @@
 import { storiesOf } from '@storybook/react';
 import colorPalette from './color-palette'
+import typographyAll, { BlueThemeText, DarkThemeText, LightThemeText } from './typography'
 
 const DesignElements = () => storiesOf('Design Elements', module)
   .add('Colors', colorPalette)
+  .add('Typography - All Texts', typographyAll)
+  .add('Typography - Light Theme', LightThemeText)
+  .add('Typography - Dark Theme', DarkThemeText)
+  .add('Typography - Blue Theme', BlueThemeText)
+
 
 export default DesignElements
