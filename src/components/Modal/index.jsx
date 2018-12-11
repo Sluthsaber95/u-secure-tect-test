7c1c945acc1184dc28ca3b10fad392896dc9c8cf
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-11 12:49:06 +0000

    Feat Modal Init

---
+++ src/components/Modal/index.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import styled from '@emotion/styled'
+
+import Form from '../Form'
+import Button from '../Button'
+
+const ModalBackground = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background: black;
+  opacity: 0.9;
+`
+
+class Model extends Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    const { addUser, modalOpen } = this.props
+    return (
+      <section>
+        {
+          modalOpen && 
+          <ModalBackground>
+            <Form addUser={addUser} modalClose={this.handleClose} />
+          </ModalBackground>
+        }
+        <Button iconPresent={true} onClick={this.props.handleOpen}>Add User</Button>
+      </section>
+    )
+  }
+}
+
+export default Model;
