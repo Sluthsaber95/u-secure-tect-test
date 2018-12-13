015f0717c95a33a3eec99a2eddc6faa66cc2c7e2
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-13 16:36:27 +0000

    Feat  Description init

---
+++ src/screens/settings/common/Description.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled'
+
+import globalStyles from '../../../globalStyles'
+
+const { color, typography } = globalStyles()
+const { grayDarkSlate } = color
+const { mdLight } = typography
+
+export const Description = styled.p`
+  font: ${mdLight};
+  color: ${grayDarkSlate};
+  letter-spacing: 2px;
+  margin-left: ${props => props.firstChild ? '0px' : '15px'};
+  margin-right: 15px;
+`
