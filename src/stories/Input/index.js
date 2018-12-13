b08e639526d26dbfc2ee50d22d2d92a9b1af9bc7
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-13 16:24:18 +0000

    Doc Input display able to handle placeholder props

--- src/stories/Input/index.js
+++ src/stories/Input/index.js
@@ -3,6 +3,6 @@ import { storiesOf } from '@storybook/react'
 import Input from '../../components/Input'
 
 const StoriesInput = () => storiesOf('Input', module)
-  .add('Input', () => <Input />)
+  .add('Input', () => <Input placeholder="Write a message"/>)
 
 export default StoriesInput
