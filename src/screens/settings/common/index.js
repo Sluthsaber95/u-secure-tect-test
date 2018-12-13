59c32d29449d7cf8d89a84312bd9d27974ac256b
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-13 16:39:40 +0000

    Feat settings/common init

---
+++ src/screens/settings/common/index.js
@@ -0,0 +1,15 @@
+import { ButtonYesNo } from './ButtonYesNo'
+import { Description } from './Description'
+import { DescriptionBold } from './DescriptionBold'
+import { Row } from './Row'
+import { SectionWrapper } from './SectionWrapper'
+import { SubTitle } from './SubTitle'
+
+export {
+  ButtonYesNo,
+  Description,
+  DescriptionBold,
+  Row,
+  SectionWrapper,
+  SubTitle,
+}
