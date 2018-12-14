4d2edc1f7beb15a2aa431e77494b0fb3187fdd15
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-14 06:58:59 +0000

    Feat Users added content and functionality

--- src/screens/settings/Users/index.jsx
+++ src/screens/settings/Users/index.jsx
@@ -1,7 +1,29 @@
 import React, { Component } from 'react'
 
+import Table from '../../../components/Table'
+import { Description, DescriptionBold, Row } from '../common'
+
 export default class Users extends Component {
   render() {
-    return <div>Users Route</div>
+    return (
+      <section>
+        <Description firstChild={true}>
+          Access Level: <DescriptionBold>Admin</DescriptionBold>
+        </Description>
+        <Description firstChild={true} smallText={true}>
+          This access level enables full rights to 
+          <ul>
+            <li>View all User details</li>
+            <li>Add Users</li>
+            <li>Remove Users</li>
+            <li>Edit Users' details</li>
+            <li>Send Users' password reset links</li>
+          </ul>
+        </Description>
+        <article style={{ marginTop: 50 }}>
+          <Table />
+        </article>
+      </section>
+    )
   }
 }
