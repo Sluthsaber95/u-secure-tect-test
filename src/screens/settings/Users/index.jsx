3d096320ec2c06f318356da906c83fe58ae75031
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-14 09:47:27 +0000

    Feat Users Removal of unrequired text

--- src/screens/settings/Users/index.jsx
+++ src/screens/settings/Users/index.jsx
@@ -1,23 +1,18 @@
 import React, { Component } from 'react'
 
 import Table from '../../../components/Table'
-import { Description, DescriptionBold, Row } from '../common'
+import { Description } from '../common'
 
 export default class Users extends Component {
   render() {
     return (
       <section>
-        <Description firstChild={true}>
-          Access Level: <DescriptionBold>Admin</DescriptionBold>
-        </Description>
         <Description firstChild={true} smallText={true}>
-          This access level enables full rights to 
+          What you can do ...
           <ul>
             <li>View all User details</li>
-            <li>Add Users</li>
-            <li>Remove Users</li>
+            <li>Add/Remove Users</li>
             <li>Edit Users' details</li>
-            <li>Send Users' password reset links</li>
           </ul>
         </Description>
         <article style={{ marginTop: 50 }}>
