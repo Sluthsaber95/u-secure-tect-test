1ea5c33331f68815c571872232684f32299703af
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-14 01:45:00 +0000

    Feat App includes routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 import styled from '@emotion/styled'
 
 import globalStyles from './globalStyles'
-import SettingsUSecure from './screens/settings/USecure'
+import NavMenu from './components/NavMenu'
+import { SettingSecurity, SettingUSecure, SettingUsers }from './screens/settings'
 
 const { color, typography } = globalStyles()
 const { grayDarkSlate } = color
@@ -20,19 +23,24 @@ const Title = styled.h1`
   font: ${xlReg};
   color: ${grayDarkSlate};
   margin-top: 0;
-  padding: 50px 0 ;
+  padding-top: 50px;
   letter-spacing: 1px;
 `
 
 class App extends Component {
   render() {
     return (
-      <Fragment>
+      <Router>
         <AppWrapper>
           <Title>Settings</Title>
-          <SettingsUSecure />
+          <NavMenu />
+          <Switch>
+            <Route exact path="/settings/u-secure" render={props => <SettingUSecure title="Settings" />} />
+            <Route path="/settings/security" render={props => <SettingSecurity title="Settings" />} />
+            <Route path="/settings/users" render={props => <SettingUsers title="Settings" />} />
+          </Switch>
         </AppWrapper>
-      </Fragment>
+      </Router>
     );
   }
 }
