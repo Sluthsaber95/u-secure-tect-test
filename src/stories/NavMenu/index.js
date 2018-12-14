459fee72d4799f9e954aec555faab33f1cb87010
Anthony Tran <anthonytran.dev@gmail.com>
2018-12-14 01:46:18 +0000

    Bug Fix NavMenu corrected the import path

--- src/stories/NavMenu/index.js
+++ src/stories/NavMenu/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import NavMenu from '../../components/MenuNav'
+import NavMenu from '../../components/NavMenu'
 
 const StoriesNavMenu = () => storiesOf('NavMenu', module)
   .add('NavMenu', () => <NavMenu />)
